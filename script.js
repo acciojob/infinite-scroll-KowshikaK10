d9fd3551c789f9e94056acaddbb8db812f2cc13f
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-03 18:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", () => {
+let total=10;
+let olList=document.getElementById('infi-list');
 
+    for(let i=1;i<=total;i++){
+        let li=document.createElement('li');
+        li.innerText=`Item ${i}`;
+        olList.append(li);
+    }
+	olList.addEventListener('scroll',()=>{
+		const scrollTop = olList.scrollTop;
+	    const scrollHeight = olList.scrollHeight;
+	    const clientHeight = olList.clientHeight;
+		  if (scrollTop + clientHeight >= scrollHeight) {
+			  for(let i=1;i<=2;i++){
+			        let li=document.createElement('li');
+			        li.innerText=`Item ${++total}`;
+			        olList.append(li);
+			    }
+		  }
+	})
+});
